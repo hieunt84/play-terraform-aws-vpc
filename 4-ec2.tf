df8865666f6c504b0762cdca4e181bb68c7fa2be
Focus <hieunt9@gmail.com>
2022-05-23 10:27:31 +0700

    delete EC2 test

--- 4-ec2.tf
+++ 4-ec2.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_instance" "test_server" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
@@ -6,3 +7,4 @@ resource "aws_instance" "test_server" {
     Name = "${var.project}-ec2-test"
   }
 }
+*/
