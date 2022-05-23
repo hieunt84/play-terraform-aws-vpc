b066ce0193633d9323676019067c8201456f07a4
Focus <hieunt9@gmail.com>
2022-05-23 17:23:38 +0700

    create IAM Role for ec2 access SSM

--- 4-ec2.tf
+++ 4-ec2.tf
@@ -1,4 +1,31 @@
+
+# EC2 IAM Role access System Manager
+resource "aws_iam_role" "ec2tosm" {
+  name = "${var.project}-ec2tosm-Role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_AmazonEC2RoleforSSM" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2RoleforSSM"
+  role       = aws_iam_role.ec2tosm.name
+}
+
 /*
+#############################################
 resource "aws_instance" "test_server" {
   ami           = "ami-0ca285d4c2cda3300"
   instance_type = "t2.micro"
