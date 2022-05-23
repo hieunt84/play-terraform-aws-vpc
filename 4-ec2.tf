bd42838aabaf5be596b4533605bbd826e8abaaf0
Focus <hieunt9@gmail.com>
2022-05-23 11:08:45 +0700

    create EC2 test

--- 4-ec2.tf
+++ 4-ec2.tf
@@ -1,10 +1,9 @@
-/*
 resource "aws_instance" "test_server" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
+  subnet_id = aws_subnet.public[0].id
 
   tags = {
     Name = "${var.project}-ec2-test"
   }
 }
-*/
